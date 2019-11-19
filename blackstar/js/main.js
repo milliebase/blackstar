4cce87c12df58a5546ab451ed73b4e4fe696be9f
milliebase <betsyalva@milliebase.se>
2019-11-19 01:02:01 +0100

    add transition to overlay-nav, change menu items, scroll-behavior

--- blackstar/js/main.js
+++ blackstar/js/main.js
@@ -1,12 +1,17 @@
 //Eventlistener for overlay-nav
+const body = document.querySelector('body');
 const menuButton = document.querySelector('.menu-button');
 const overlayNav = document.querySelector('.overlay-nav');
 const exitButton = document.querySelector('.overlay-nav-menu img');
 
 menuButton.addEventListener('click', () => {
   overlayNav.classList.toggle('hidden');
+  overlayNav.classList.add('transform-active');
+  body.classList.toggle('no-scroll');
 })
 
 exitButton.addEventListener('click', () => {
   overlayNav.classList.toggle('hidden');
+  overlayNav.classList.remove('transform-active')
+  body.classList.toggle('no-scroll');
 })
