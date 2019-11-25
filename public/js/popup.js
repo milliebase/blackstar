076a981fc252a33940a937ae927737e869dface4
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-11-25 14:32:28 +0100

    finished popup effect

--- public/js/popup.js
+++ public/js/popup.js
@@ -3,6 +3,8 @@ const popupBox = document.querySelector('.popup');
 const popupActivateBtn = document.querySelector('.text-sections button');
 const popupSubmitBtn = document.querySelector('.popup button');
 const popupInput = document.querySelector('.popup form input');
+const submitBtnContent = document.querySelector('.popup button span');
+console.log(submitBtnContent);
 
 popupActivateBtn.addEventListener('click', () => {
   popupActivateBtn.classList.add('button-active');
@@ -23,5 +25,24 @@ popupInput.addEventListener('blur', () => {
 popupInput.addEventListener('keyup', () => {
   if (event.target.value.length > 2) {
     popupSubmitBtn.classList.add('button-active');
+  } else {
+      popupSubmitBtn.classList.remove('button-active');
   }
+})
+
+popupSubmitBtn.addEventListener('click', () => {
+    if(popupSubmitBtn.classList.contains('button-active')) {
+        submitBtnContent.classList.add('hidden');
+        popupInput.value = '';
+        popupInput.placeholder='';
+        setTimeout(() => {
+            popupSubmitBtn.innerHTML = "<img src='/public/images/svg/mark.svg' alt='check-mark'>";
+
+        }, 700)
+        setTimeout(() => {
+            popupBox.classList.remove('popup-effect');
+        }, 1500)
+
+
+    }
 })
