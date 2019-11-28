2fab1a6b53eb85292110ca0afc7a482e073b10ac
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-11-28 13:00:16 +0100

    arrow-down dissapears on scroll

---
+++ public/js/image-section1.js
@@ -0,0 +1,11 @@
+const arrowDown = document.querySelector('.image-section1 .arrow-down');
+console.log(arrowDown);
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 115) {
+        arrowDown.classList.add('hidden');
+    } else {
+        arrowDown.classList.remove('hidden');
+    }
+    console.log(window.pageYOffset);
+})
