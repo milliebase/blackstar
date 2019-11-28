674bdcb1460a7d2bf1fd59e92b7ea22cd75d0f2d
milliebase <betsyalva@milliebase.se>
2019-11-28 15:46:45 +0100

    update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -111,3 +111,75 @@ colorPickers.forEach(colorPicker => {
         handleColorPicker(event.currentTarget);
     })
 })
+
+
+//.IMAGE-SECTION3
+const leftArrow = document.querySelector('.arrow-holder-left');
+const rightArrow = document.querySelector('.arrow-holder-right');
+const imgHolder = document.querySelector('.img-inner-holder');
+const image = document.querySelector('.img-inner-holder img');
+const imageWidth = image.clientWidth;
+
+const slideToRight = () => {
+        if (imgHolder.scrollLeft === 0) {
+            imgHolder.scrollTo({
+                top: 0,
+                left: imageWidth,
+                behavior: 'smooth'
+            });
+        }
+
+        if (imgHolder.scrollLeft === imageWidth) {
+            imgHolder.scrollTo({
+                top: 0,
+                left: imageWidth * 2,
+                behavior: 'smooth'
+            });
+        }
+}
+
+const slideToLeft = () => {
+    const thirdImage = imageWidth * 2
+
+    if (imgHolder.scrollLeft === thirdImage) {
+        imgHolder.scrollTo({
+            top: 0,
+            left: imageWidth,
+            behavior: 'smooth'
+        });
+    }
+
+    if (imgHolder.scrollLeft === imageWidth) {
+        imgHolder.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+}
+
+const handleSlide = (e) => {
+    if (e.toElement.alt === 'Right arrow') {
+        slideToRight();
+    }
+
+    if (e.toElement.alt === 'Left arrow') {
+        slideToLeft();
+    }
+}
+
+leftArrow.addEventListener('click', (e) => {
+    handleSlide(e);
+});
+
+rightArrow.addEventListener('click', (e) => {
+    handleSlide(e);
+});
+
+
+//DOWNLOAD
+
+const downloadButton = document.querySelector('.download-brochure button');
+
+console.log(downloadButton);
+
